d3e00991ebe498483dcf345afa59d7ee8dd2dd8f
Arthur Kwok <951224758arthur@gmail.com>
2020-09-28 15:56:25 +0800

    add interval singer

--- src/singer.py
+++ src/singer.py
@@ -67,8 +67,8 @@ class Singer(object):
            value[1] > len(self.chord_progression.split("\n"))-1 or \
            value[2] > len(self.chord_progression.split("\n"))-1:
            raise ValueError(f"Invalid pattern progression, must be smaller than chord progression : {value}")
-    
-    
+
+
     #
     # class methods
     #
@@ -82,9 +82,9 @@ class Singer(object):
         rand_vol: range of random volume (0 to 127)
         rand_trig: a possibility of notes being muted, 0 to 1 (0 will trigger all notes, 1 mutes all)
 
+        recommend setting: speed=4/8/16, rand_vol=10, rand_trig=0.2
         """
         default_volume = 90
-
 
         for current_chord in self.chords.elements:
             chord_tones = [pitch.name for pitch in current_chord.pitches]
@@ -105,6 +105,35 @@ class Singer(object):
                 self.melody.append(n)
 
 
+    def sing_interval(self, speed, rand_vol, rand_trig):
+        """
+        Sing according to interval with the previous note. closer note will have higher probability.
+        """
+        default_volume = 90
+
+        for current_chord in self.chords.elements:
+            chord_tones = [pitch.name for pitch in current_chord.pitches]
+            singable_pitchs = []
+            for pitch in self.possible_pitches:
+                if pitch.name in chord_tones:
+                    singable_pitchs.append(pitch.nameWithOctave)
+
+            for i in range(speed):
+                if np.random.rand() < rand_trig:
+                    n = m2.note.Rest()
+                else:
+                    if len(self.melody.notes) == 0:
+                        current_pitch = np.random.choice(singable_pitchs)
+                    else:
+                        interval_p = self.interval_reversed_p(self.melody.notes[-1].pitch, singable_pitchs)
+                        current_pitch = np.random.choice(singable_pitchs, p=interval_p)
+                    n = m2.note.Note(current_pitch)
+                    n.volume = m2.volume.Volume(velocity=default_volume+int(rand_vol*(2*np.random.rand()-1)))
+                n.duration = m2.duration.Duration(4/speed)
+
+                self.melody.append(n)       
+
+
     def export_midi(self, midi_path, write_chords=False):
         """
         """
@@ -113,10 +142,36 @@ class Singer(object):
         self.s.write("midi", midi_path)
         print(f"midi file written at {midi_path}")
 
+
+    @staticmethod
+    def interval_reversed_p(target_pitch, pitch_list, prob_factor=2, prob_offset=5)->list:
+        """
+        calculate the interval of the pitch to each element in the pitch list.
+        returns a normalized probability of each note, closer note has higher probability.
+
+        Parameters
+        ----------
+        target_pitch: music21.interval.Interval object.
+        pitch_list: list of strings, each string is a pitch name, e.g. "G4"
+        prob_factor: the index of reverse probability. if bigger, the closer note will have higher probability.
+        prob_offset: offset when calculating inversed probability.
+
+        Return
+        ------
+        interval_p: the normalized probability of each note.
+        """
+        interval_to_rf = np.array([np.abs(m2.interval.Interval(target_pitch, m2.pitch.Pitch(p)).semitones) for p in pitch_list])
+        interval_p = 1 / (interval_to_rf+prob_offset)
+        interval_p = interval_p ** prob_factor
+        interval_p = interval_p / np.sum(interval_p)
+        return interval_p
+
+
+
 if __name__ == "__main__":
     my_singer = Singer(tempo=110, key="D", time_signature="4/4", 
                        chord_progression="D\nBm\nG\nA7\nD\nBm\nG\nA7\nD\nBm\nG\nA7\nD\nBm\nG\nA7\n",
                        pattern_progression=[5, 9, 13])
     
-    my_singer.sing_chord(speed=8, rand_vol=10, rand_trig=0.2)
+    my_singer.sing_interval(speed=8, rand_vol=10, rand_trig=0.2)
     my_singer.export_midi("../singer_output.mid", write_chords=True)
